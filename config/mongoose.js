2e73bd63005e093c9f2c841f33eb653400d96551
Ayush Singh <ayushsingh4y@gmail.com>
2023-05-11 10:21:29 +0530

    Added Srv string

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/Issue-Tracker');
+mongoose.connect('mongodb+srv://ayushsingh4y:IRRWs0Ux3qGCh9MU@cluster0.xdciasq.mongodb.net/Issue?retryWrites=true&w=majority');
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
